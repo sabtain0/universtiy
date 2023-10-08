e7d51e1f31804f1dcfabdaa18f8d650cf2dadf7c
sabtain0 <138502598+sabtain0@users.noreply.github.com>
2023-10-08 15:02:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let navlinks = document.getElementById("nav-links")
+function showmenu(){
+    navlinks.style.display="block";
+    navlinks.style.transition = "1s"
+}
+function hidemenu(){
+    navlinks.style.display="none";
+    navlinks.style.transition = "1s"
+}
+
+console.log("jelo")
